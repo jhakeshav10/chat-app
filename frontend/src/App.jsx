f482a5576f4501dabb62bfd935cec37afe7b78c4
Keshav Jha <keshav17082003@gmail.com>
2024-04-11 14:33:53 +0530

    2nd part done - UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css';
-import Login from './pages/login/Login';
-import SingUp from './pages/signup/SingUp';
 import Home from './pages/home/Home';
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <SingUp /> */}
       <Home />
     </div>
   );
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TbLogout2 } from "react-icons/tb";
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'>
+        <TbLogout2 className='w-6 h-6 text-red-300 cursor-pointer' />
+    </div>
+  )
+}
+
+export default LogoutButton
