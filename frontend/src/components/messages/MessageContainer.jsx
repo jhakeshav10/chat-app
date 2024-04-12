6ad9391b2f8f0dd2b6ca2b378c1d9184dd420204
Keshav Jha <keshav17082003@gmail.com>
2024-04-12 22:17:36 +0530

    get conversations and search function implemented + basic ui changes

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TbMessages } from "react-icons/tb";
+import useConversation from '../../zustand/useConversation';
 
 
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const {selectedConversation, setSelectedConversation} = useConversation()
+
+    useEffect(() => {
+        // clean-up function (unmounts)
+        return () => setSelectedConversation(null)
+    }, [setSelectedConversation])
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-           {noChatSelected ? <NoChatSelected /> : (
+           {!selectedConversation ? <NoChatSelected /> : (
              <>
              {/* Header */}
              <div className='bg-slate-500 px-4 py-2 mb-2'>
-                 <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John Doe</span>
+                 <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
              </div>
 
              <Messages />
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { TbLogout2 } from "react-icons/tb";
+import useLogout from '../../hooks/useLogout';
 
 const LogoutButton = () => {
+
+  const {loading,logout}= useLogout()
   return (
     <div className='mt-auto'>
-        <TbLogout2 className='w-6 h-6 text-red-300 cursor-pointer' />
+      {!loading ? (
+         <TbLogout2 className='w-6 h-6 text-red-300 cursor-pointer' 
+         onClick={logout}
+       />
+      ) : (
+        <span className='loading loading-spinner'></span>
+      )}
     </div>
   )
 }
